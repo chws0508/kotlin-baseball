60996016ec46e6cb3de77d59c2781727eb75fa98
ChoiWooSuk <wooseokchoi0508@gmail.com>
2022-12-15 16:23:28 +0900

    feat: 게임 시작문구와 게임 결과 문구 출력 기능 추가

---
+++ src/main/kotlin/baseball/OutputView.kt
@@ -0,0 +1,28 @@
+package baseball
+
+class OutputView {
+
+    fun printStartMessage() {
+        println(GAME_START_MESSAGE)
+    }
+
+    fun printBall(ball: Int) {
+        if (ball != 0) print("${ball}${BALL} ")
+    }
+
+    fun printStrike(strike: Int) {
+        if (strike != 0) print("${strike}${STRIKE}")
+        println()
+    }
+
+    fun printGameResult() {
+        println(GAME_END)
+    }
+
+    companion object {
+        const val GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다."
+        const val BALL = "볼"
+        const val STRIKE = "스트라이크"
+        const val GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+    }
+}
